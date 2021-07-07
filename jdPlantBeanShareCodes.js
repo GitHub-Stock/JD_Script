70d90933279f46e6d89491dd9756b7f35f01bad7
GitHub-Stock <1335294455@qq.com>
2021-07-07 11:14:45 +0800

    add

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -6,7 +6,10 @@
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let PlantBeanShareCodes = ['']
+let PlantBeanShareCodes = [
+  '@',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '@',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
 
 // 从日志获取互助码
 // const logShareCodes = require('./utils/jdShareCodes');
@@ -26,7 +29,7 @@ if (process.env.PLANT_BEAN_SHARECODES) {
     PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split();
   }
 } else {
-  console.log(`由于您环境变量(PLANT_BEAN_SHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(`去设置变量(PLANT_BEAN_SHARECODES)不同好友中间用@符号隔开`)
 }
 for (let i = 0; i < PlantBeanShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
