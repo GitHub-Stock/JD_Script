a27e8e65222dce922762f801f3712f099acf45ab
lermite <1335294455@qq.com>
2021-07-23 16:38:12 +0800

    add

--- jd_dreamFactorytuan.js
+++ jd_dreamFactorytuan.js
@@ -23,7 +23,7 @@
   const notify = $.isNode() ? require('./sendNotify') : '';
   const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
   const openTuanCK = $.isNode() ? (process.env.OPEN_DREAMFACTORY_TUAN ? process.env.OPEN_DREAMFACTORY_TUAN : '1,2,3,4'):'1,2,3,4';
-  const helpFlag = true;//是否参考作者团
+  const helpFlag = false;//是否参考作者团
   let tuanActiveId = ``;
   let cookiesArr = [], cookie = '', message = '';
   $.tuanIds = [];
